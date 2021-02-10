18ad08a942e446ad0b1dc0394aa0ab22a1c68744
Richard Pasquiou <services@pasquiou.fr>
2021-02-10 21:14:02 +0100

    clean markup

--- next/components/skeleton.js
+++ next/components/skeleton.js
@@ -9,7 +9,6 @@ import Splash from './Splash';
 import Coords from './Coords';
 import { Section } from './designSystem/layout';
 
-
 export const appendSiteTitle = ' - Homecano';
 
 export default function Skeleton({ children }) {
@@ -21,8 +20,7 @@ export default function Skeleton({ children }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Homecano, Garage à domicile" />
       </Head>
-      {/* <Splash> */}
-      {/* <div className="bg-header bg-cover bg-green-200"> */}
+
       <div className="relative min-h-half">
         <div className="absolute inset-0 overflow-hidden bg-blue-400 bg-opacity-40">
           <Img
@@ -35,24 +33,19 @@ export default function Skeleton({ children }) {
             src="/images/jesse-bowser-c0I4ahyGIkA-unsplash.jpg"
             alt=""
           />
-          <div className="absolute -bottom-16 -left-5 -right-5 h-40 bg-white transform  rotate-2"></div>
+          <div className="absolute -bottom-16 -left-5 -right-5 h-24 bg-white transform  rotate-2" />
         </div>
         <Header />
       </div>
 
-        <SkipNavContent
-          style={{
-            scrollMarginTop: '12rem',
-            position: 'relative',
-          }}
-        >
-          <main>{children}</main>
-        </SkipNavContent>
-
-      {/* <Section>
-          <Coords />
-        </Section> */}
-      {/* </Splash> */}
+      <SkipNavContent
+        style={{
+          scrollMarginTop: '12rem',
+          position: 'relative',
+        }}
+      >
+        <main>{children}</main>
+      </SkipNavContent>
 
       <Footer />
     </>
