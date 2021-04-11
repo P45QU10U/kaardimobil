83ad97535ff02b2ed81c34260727b5e705e5d5b1
Richard Pasquiou <services@pasquiou.fr>
2021-04-11 20:30:20 +0200

    Removed background header

--- next/components/Header.js
+++ next/components/Header.js
@@ -6,7 +6,6 @@ import Coords from './Coords';
 
 import { Section } from './designSystem/layout';
 import { BackgroundImage } from './BackgroundImage';
-import Promotions from './Promotions';
 
 export default function Header() {
   return (
@@ -14,12 +13,12 @@ export default function Header() {
       <div className="relative">
         {/* <div className="absolute -top-16 -left-10 -right-10 h-1/2 bg-gradient-to-tl from-orange-500 to-orange-300 transform  rotate-2" /> */}
         {/* <BackgroundImage bgopacity="bg-opacity-100" bgcolor="bg-orange-400" /> */}
-        <BackgroundImage src="/images/jesse-bowser-c0I4ahyGIkA-unsplash.jpg" />
+        {/*         <BackgroundImage src="/images/jesse-bowser-c0I4ahyGIkA-unsplash.jpg" /> */}
 
         <header className={classnames(maxWidthSections, 'relative')}>
           <Menu />
         </header>
-        <Promotions />
+
         {/* <Coords /> */}
       </div>
     </div>
